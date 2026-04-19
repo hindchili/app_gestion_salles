e7b16076e6c555cb30bcf6c0dc4fa21e6fb17ba0
hind <300151558@monboreal.ca>
2026-04-19 19:59:45 -0400

    Ajout couche Service

---
+++ services/services_salle.py
@@ -0,0 +1,24 @@
+from Data.dao_salle import DataSalle
+
+class ServiceSalle:
+
+    def __init__(self):
+        self.dao = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if salle.capacite < 1:
+            return False
+        self.dao.insert_salle(salle)
+        return True
+
+    def modifier_salle(self, salle):
+        self.dao.update_salle(salle)
+
+    def supprimer_salle(self, code):
+        self.dao.delete_salle(code)
+
+    def rechercher_salle(self, code):
+        return self.dao.get_salle(code)
+
+    def recuperer_salles(self):
+        return self.dao.get_salles()
