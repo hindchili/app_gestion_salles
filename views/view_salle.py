3362f9972688c006ed2cee8a09de02f4616b659c
hind <300151558@monboreal.ca>
2026-04-19 20:37:00 -0400

    Affichage des salles au lancement

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import ttk
 from services.services_salle import ServiceSalle
 from models.salle import Salle
 
@@ -20,9 +21,26 @@ class ViewSalle(ctk.CTk):
 
         self.cap = ctk.CTkEntry(self)
         self.cap.pack()
+        self.tree = ttk.Treeview(self, columns=("code", "desc", "cat", "cap"), show="headings")
+
+        self.tree.heading("code", text="Code")
+        self.tree.heading("desc", text="Description")
+        self.tree.heading("cat", text="Categorie")
+        self.tree.heading("cap", text="Capacite")
+
+        self.tree.pack(fill="both", expand=True)
+        self.lister()
 
         ctk.CTkButton(self, text="Ajouter", command=self.ajouter).pack()
 
     def ajouter(self):
         s = Salle(self.code.get(), self.desc.get(), self.cat.get(), int(self.cap.get()))
         self.service.ajouter_salle(s)
+
+def lister(self):
+    self.tree.delete(*self.tree.get_children())
+
+    liste = self.service.recuperer_salles()
+
+    for s in liste:
+        self.tree.insert("", "end", values=(s.code, s.description, s.categorie, s.capacite))
